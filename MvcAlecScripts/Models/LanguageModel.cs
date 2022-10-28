45475583deec4590ed9f1fdaf3a0e334f76671c6
Alec Sears <76724804+searsam1@users.noreply.github.com>
2022-10-28 17:30:02 -0600

    Delete MvcAlecScripts directory

--- MvcAlecScripts/Models/LanguageModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MvcAlecScripts.Models;
-
-public class LanguageModel
-{
-    public string? Language { get; set; }
-    public string? ReleaseDate { get; set; }
-    public string? Use { get; set; }
-    public string? Inventor { get; set; }
-}
