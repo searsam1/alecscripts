fbf335b6ab35d6fb5bfec8153f116008dd85b8d5
Alec <amsears60441@gmail.com>
2022-11-14 22:37:58 -0700

    change h1 -> h2, h2 -> h3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,11 +40,4 @@ public class HomeController : Controller
         return View();
     }
 
-    // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    // public IActionResult Error()
-    
-    // {
-    //     ViewData["controller"] = controllerName;
-    //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    // }
 }
