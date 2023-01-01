56e252463fe172cc99f6d6c7382a1495a8ef1fe9
Alec <amsears60441@gmail.com>
2022-12-31 20:06:07 -0700

    Added AddModel, AddViews, WorkWithdb

--- Controllers/DotnetController.cs
+++ Controllers/DotnetController.cs
@@ -47,5 +47,30 @@ namespace MvcMovie.Controllers
         return View();
     }
     
+    public IActionResult AddViews()
+    {   
+        ViewData["creationDate"] = "Thu Dec 29 05:33:23 2022 (GMT-7)";
+        ViewData["controller"] = controllerName;
+        ViewData["title"] = "Add Views";
+        return View();
+    }
+    
+    public IActionResult AddModel()
+    {   
+        ViewData["creationDate"] = "Fri Dec 30 08:03:45 2022 (GMT-7)";
+        ViewData["controller"] = controllerName;
+        ViewData["title"] = "Add Model";
+        return View();
+    }
+    
+    public IActionResult WorkWithADatabase()
+    {   
+        ViewData["creationDate"] = "Fri Dec 30 14:15:06 2022 (GMT-7)";
+        ViewData["controller"] = controllerName;
+        ViewData["title"] = "Work With A Database";
+        return View();
+    }
+    
+    
     }
 }
