2110e242375da7b343f3d6fe57cc7b4f2e87245a
Alec <amsears60441@gmail.com>
2023-01-02 01:19:58 -0700

    fixed default route

--- Program.cs
+++ Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Dotnet}/{action=AddController}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
