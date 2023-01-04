1dd181f160dd39f1255aeb702fdefe1d7977144d
Alec <amsears60441@gmail.com>
2023-01-03 22:46:27 -0700

    deleted unnecessary files

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -206,5 +206,20 @@ function getColorArr(x) {
   }
 
 
+  function alternateTableColor(){
+    let tables = document.querySelectorAll("TABLE");
 
+    for (let table of tables){
+        let trs = table.querySelectorAll("tr")
+        i = -1; 
+        for (let tr of trs){
+            i++; 
+            if (i % 2){
+                tr.style.backgroundColor = "rgb(0,0,0,.05)";
+            }
+            
+        }
+    }
+}
+alternateTableColor()
 
