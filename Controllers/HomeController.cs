31c28b508ddfed625a9a2fcbfc6dfc828cd9a3e5
Alec <amsears60441@gmail.com>
2023-01-08 20:59:58 -0700

    House Keeping

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography; // RandomNumberGenerator.GetInt32
 using MvcAlecScripts.Models;
 
 
@@ -19,10 +20,29 @@ namespace MvcAlecScripts.Controllers
         }
 
         
+        public static string RandomQuote(){
+            string[] quotes = {
+                "\"God created war so that Americans would learn geography.\" - MarkTwain",
+                "\"I know not with what weapons World War III will be fought, but World War IV will be fought with sticks and stones.\" - Albert Einstein",
+                "“The true soldier fights not because he hates what is in front of him, but because he loves what is behind him.” ― G.K. Chesterton",
+                "“Only the dead have seen the end of war.” ― Plato",
+                "“Older men declare war. But it is youth that must fight and die.” ― Herbert Hoover",
+                "“The supreme art of war is to subdue the enemy without fighting.” ― Sun Tzu, The Art of War",
+                "“Your time is limited, so don’t waste it living someone else’s life.” - Steve Jobs",
+                "“In the old world, you devoted 30% of your time to building a great service and 70% of your time to shouting about it. In the new world, that inverts.” ― Jeff Bezos"
+            };
+            int randNum = RandomNumberGenerator.GetInt32(quotes.Length);
+            return quotes[randNum];
+        }
+
         public IActionResult Index()
         {
             ViewData["controller"] = controllerName;
-            ViewData["title"] = "";
+            ViewData["title"] = "Alec Scripts Home";
+
+            ViewData["RandomQuote"] = RandomQuote();
+            
+
             return View();
         }
 
