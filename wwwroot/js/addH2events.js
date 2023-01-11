1b777715aa2b5345af22d4ae647befbb8403cecd
Alec <amsears60441@gmail.com>
2023-01-11 04:52:45 -0700

    added yellow highlight on h2 click

---
+++ wwwroot/js/addH2events.js
@@ -0,0 +1,42 @@
+function reset(h2){
+    h2.style.background = "white"
+    h2.textContent = h2.textContent.replace("⤷", "")
+}
+
+
+function copy() {
+    
+    let h2s = document.querySelectorAll("H2");
+
+    h2s.forEach(h2 => {
+        reset(h2)
+    }); 
+    
+    // Get the text field
+    let copyText = window.location.href
+    copyText = copyText.split("#")[0]
+    let ID = "#" + document.getElementById(this.id).id
+    this.style.background = "rgb(255,255,0,.3)"
+    
+    if (!this.textContent.includes("⤷")){
+        this.textContent = "⤷" + this.textContent 
+    }
+    else{
+        this.textContent = this.textContent.replace("⤷", "")
+        this.style.background = "inherit"
+    }
+    copyText += ID;
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText);
+  }
+
+function addH2events(){
+
+    let h2s = document.querySelectorAll("H2");
+    h2s.forEach(h2 => {
+        h2.addEventListener("click", copy, false);
+    });
+
+
+}
+addH2events();
