7e3db26268013dff60b4c1f15e006cf30f7ce870
Alec <amsears60441@gmail.com>
2023-01-12 15:01:31 -0700

    put js navlinkSelected function in sep. file

---
+++ wwwroot/js/navlinkSelected.js
@@ -0,0 +1,16 @@
+function navlinkSelected(){
+    let txt;
+    let navbar = document.querySelector(".collapse.navbar-collapse").querySelector("UL")
+    
+    // viewdataName == @viewData["Controller"]
+    // from _layout.cshtml
+    let controller = viewdataName.toLowerCase().replace("c&#x2b;&#x2b;", "c++").replace("js", "javascript")
+    let t = '@ViewData["title"]'.trim(" ").toLowerCase()
+    for (let li of navbar.querySelectorAll("LI")) {
+        txt = li.textContent.trim(" ").toLowerCase();
+        if (txt == controller || txt == t) {
+            li.setAttribute("class", "navlinkSelected")
+        }
+    }
+}
+navlinkSelected()
