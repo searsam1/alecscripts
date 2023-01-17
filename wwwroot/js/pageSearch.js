9fa1199ca3758d0f90b6ec9f03d76345df75f8ae
Alec <amsears60441@gmail.com>
2023-01-16 20:47:27 -0700

    style changes

--- wwwroot/js/pageSearch.js
+++ wwwroot/js/pageSearch.js
@@ -1,5 +1,38 @@
 
+function editOpacity(iter, text){
+    
+    iter.forEach(element => {
+        if (! element.textContent.toLowerCase().includes(text.toLowerCase())){
+            element.style.opacity = "20%"
+            element.style.background = "white"
+        }
+        else {
+            element.style.background = "yellow"
+            element.style.opacity = "100%"
+        }
+    });
+}
+
+function f() {
+    let allP = document.querySelectorAll("P");
+    let allH2 = document.querySelectorAll("H2");
+    let allH3 = document.querySelectorAll("H3");
+    let allCode = document.querySelectorAll("PRE");
+    let text = this.value;
+    if (!text){
+        location.reload();
+        return;
+    }
+
+    editOpacity(allP, text)
+    editOpacity(allH2, text)
+    editOpacity(allH3, text)
+    editOpacity(allCode, text)
+}
+
 function pageSearch(){
-    console.log("t")
+
+    let searchBar = document.querySelector("#searchBar")
+    searchBar.addEventListener("change", f, false)
 }
 pageSearch()
