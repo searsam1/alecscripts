19f508d7a3d7116fe95012e5552a9b6c9895c045
Alec <amsears60441@gmail.com>
2023-01-29 07:55:56 -0700

    clean up activate left and add new assign h3h2 script

--- wwwroot/js/activateLeft.js
+++ wwwroot/js/activateLeft.js
@@ -1,39 +1,40 @@
 function activate_left() {
-    let h2s = document.querySelectorAll("H2");
-    let h3s = document.querySelectorAll("H3");
-    let sidebar = document.querySelector("#rightSideList");
-    for (let h2 of h2s) {
-        if (h2.getAttribute("class") != "ignore") {
-            h2.setAttribute('class', 'header')
-        }
-    }
-    for (let h3 of h3s) {
-        if (h3.getAttribute("class") != "ignore") {
-            h3.setAttribute('class', 'header header-3')
-        }
-    }
+    
+    /* 
+        Open the left sidebar to the right section, 
+        and highlight the right page
+
+        Ex. 
+            Section -> CSS
+            Page -> CSS Home    
+    */ 
+    
+    // controller (java, css, etc..)
     let c = document.querySelector("H1").textContent.split(":")[0]
     let btns = document.querySelector("#leftSidebar").querySelector("UL").querySelectorAll('button')
     for (let btn of btns) {
         if (btn.textContent.trim(" ") == c) {
+            // open sidebar
             btn.ariaExpanded = "true"
             btn.parentElement.querySelector("div").setAttribute('class', 'collapse show')
         }
     }
+    // get title of the page to 
+    // activate on the left sidebar
     let title = document.querySelector("H1").textContent.split(":")[1].replace(/ /g, "")
     let links = document.querySelector("#leftSidebar").querySelector("UL").querySelectorAll("LI");
     for (let link of links) {
+        let a  = link.querySelector('a')
+        
+        // get rid of spaces and newlines
         let res = link.textContent.replace(/ /g, "").replace(/\n/g, "")
         if (c + title == res) {
-            link.querySelector('a').style.color = 'black'
             link.style.borderBottom = "1px solid #f1f1f1"
-            link.querySelector('a').style.fontWeight = '500 '
-            link.querySelector('a').setAttribute('id', "activeLeft")
-            link.querySelector('a').setAttribute('class', "activeLeft")
-            
-            
+            a.style.color = 'black'
+            a.style.fontWeight = '500 '
+            a.setAttribute('id', "activeLeft")
+            a.setAttribute('class', "activeLeft")            
         }
     }
 }
 activate_left()
-console.info("Activate Left")
