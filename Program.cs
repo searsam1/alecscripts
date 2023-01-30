020e814c115f0f43bcb188c8dc8ee855e106788e
Alec <amsears60441@gmail.com>
2023-01-30 10:34:06 -0700

    Reverted appsettings/ appsettings.dev/ program.cs.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,7 +19,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.MapRazorPages();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
@@ -25,48 +27,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
-// =====================================
-// using Microsoft.AspNetCore.Identity;
-// using Microsoft.EntityFrameworkCore;
-// using MvcAlecScripts.Areas.Identity.Data;
-// var builder = WebApplication.CreateBuilder(args);
-// var connectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
-//     builder.Services.AddDbContext<MvcAlecScriptsIdentityDbContext>(options =>
-//         options.UseSqlServer(connectionString));
-        
-//         builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//             .AddEntityFrameworkStores<MvcAlecScriptsIdentityDbContext>();
-// var services = builder.Services;
-// var configuration = builder.Configuration;
-
-// services.AddAuthentication().AddGoogle(googleOptions =>
-//     {
-//         googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
-//         googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
-//     });
-// // Add services to the container.
-// builder.Services.AddControllersWithViews();
-
-// var app = builder.Build();
-
-// // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment())
-// {
-//     app.UseExceptionHandler("/Home/Error");
-//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//     app.UseHsts();
-// }
-
-// app.UseHttpsRedirection();
-// app.UseStaticFiles();
-
-// app.UseRouting();
-// app.UseAuthentication();
-// app.UseAuthorization();
-// app.MapRazorPages();
-// app.MapControllerRoute(
-//     name: "default",
-//     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-// app.Run();
